c0191fae87626d21ef94a6b9eed758dcc86d890f
Swathi <uppuswathi92@gmail.com>
2019-09-29 21:34:22 -0500

    latest

--- src/app/other-events/other-events.component.ts
+++ src/app/other-events/other-events.component.ts
@@ -9,7 +9,36 @@ import { DatePipe } from '@angular/common';
 })
 export class OtherEventsComponent implements OnInit {
     events: string[];
+	/*noresponseEvents: string[] = [{eventAddress: "1910 Barrymore Common, Fremont, CA 94538, USA"
+eventDate: "Monday, September 30, 2019 at 15:30"
+eventId: "37419"
+eventMsg: "please do attend"
+eventName: "swathi's birthday"
+formattedDate: "Monday, September 30, 2019"
+host: false
+	username: "william"}];*/
+	noresponseEvents: string[] = new Array();
+	notAttendingEvents: string[] = new Array();
+	attendingEvents: string[] = new Array();
+	pastEvents: string[] = new Array();
+	/*attendingEvents: string[] = [{eventAddress: "1910 Barrymore Common, Fremont, CA 94538, USA"
+eventDate: "Monday, September 30, 2019 at 15:30"
+eventId: "37419"
+eventMsg: "please do attend"
+eventName: "swathi's birthday"
+formattedDate: "Monday, September 30, 2019"
+host: false
+	username: "william"}];
+	attendingEvents: string[] = [{eventAddress: "1910 Barrymore Common, Fremont, CA 94538, USA"
+eventDate: "Monday, September 30, 2019 at 15:30"
+eventId: "37419"
+eventMsg: "please do attend"
+eventName: "swathi's birthday"
+formattedDate: "Monday, September 30, 2019"
+host: false
+	username: "william"}];*/
     eventDateTime: String = "";
+	userResponse: String = "";
     constructor(private eventsService: EventsService,
         private datePipe: DatePipe) {}
 
@@ -32,7 +61,23 @@ export class OtherEventsComponent implements OnInit {
             response => this.handleSuccessfulResponse(response),
         );
     }
-
+	//get the response whether user is attending the event or not
+    getRsvp() {
+		for(var i=0;i<this.events.length;i++){
+			var selectedDate = this.events[i]["formattedDate"];
+			var now = new Date();
+			var dt1 = Date.parse(""+new Date()),
+			dt2 = Date.parse(selectedDate);
+		   if (dt2 < dt1) {
+				this.pastEvents.push(this.events[i]);
+		   }else{
+			   this.eventsService.getRsvp(this.events[i]["eventId"]).subscribe(
+            response => this.handleSuccessfulResponse(response),
+			);
+		   }
+			
+		}		
+    }
     //handles response from http service
     handleSuccessfulResponse(response) {
         if (response != null) {
@@ -41,11 +86,63 @@ export class OtherEventsComponent implements OnInit {
                     var dateArray = response.results[i].eventDate.split(" at ");
                     var formattedDate = this.datePipe.transform(new Date(dateArray[0]), 'fullDate');
                     response.results[i].eventDate = formattedDate + " at " + dateArray[1];
+					response.results[i].formattedDate = formattedDate;
+					/*var inputDate = new Date();
+					//this.currentResponse = response.results[i];
+					if(response.results[i].formattedDate < inputDate){
+						this.pastEvents.push(response.results[i]);
+					}else{
+						this.getRsvp(response.results[i]);
+					}*/
                 }
-                this.events = response.results;
+				this.events = response.results;
+				this.getRsvp();
+				
+				//console.log(this.noresponseEvents);
+				/*this.noresponseEvents.sort((val1, val2)=> {return new Date(val1.formattedDate) - new 
+						Date(val2.formattedDate)});
+				this.notAttendingEvents.sort((val1, val2)=> {return new Date(val1.formattedDate) - new 
+				Date(val2.formattedDate)});
+				this.attendingEvents.sort((val1, val2)=> {return new Date(val1.formattedDate) - new 
+				Date(val2.formattedDate)});*/
+                
+				console.log(response.results);
+            }else if (response.service === "getRsvp") {
+                
+				var resp = response.results.split("-");
+				var eId = resp[1];
+				this.userResponse = resp[0];
+				if(this.userResponse === "no response"){
+						for(var i=0;i<this.events.length;i++){
+							if(this.events[i]["eventId"] == eId){
+							this.noresponseEvents.push(this.events[i]);
+							this.noresponseEvents.sort((val1, val2)=> {return <any>new Date(val1["formattedDate"]) - <any>new Date(val2["formattedDate"]) });
+							}
+						}
+					}else if(this.userResponse === "not attending"){
+						for(var i=0;i<this.events.length;i++){
+							if(this.events[i]["eventId"] == eId){
+							this.notAttendingEvents.push(this.events[i]);
+							this.notAttendingEvents.sort((val1, val2)=> {return <any>new Date(val1["formattedDate"]) - <any>new Date(val2["formattedDate"]) });
+							}
+						}
+						
+					}else if(this.userResponse === "attending"){
+						for(var i=0;i<this.events.length;i++){
+							if(this.events[i]["eventId"] == eId){
+							this.attendingEvents.push(this.events[i]);
+							this.attendingEvents.sort((val1, val2)=> {return <any>new Date(val1["formattedDate"]) - <any>new Date(val2["formattedDate"]) });
+							}
+						}
+						
+					}
             }
         }
 
     }
 
 }
+
+//doc with usecases
+//class and sequence diagram
+//source code, db folder
